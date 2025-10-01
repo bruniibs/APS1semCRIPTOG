9a77e0954c77bf1e598d9f5e5c0e33d791629c47
boo <studiiesboo@gmail.com>
2025-10-01 20:48:14 -0300

    primeiro commit

---
+++ files/main.py
@@ -0,0 +1,49 @@
+def cifra_cesar(texto, chave=3):
+    """Aplica a cifra de César com chave fixa = 3"""
+    resultado = ""
+    for char in texto:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            resultado += chr((ord(char) - base + chave) % 26 + base)
+        else:
+            resultado += char
+        return resultado
+
+def xor(texto, chave):
+    """Aplica XOR em cada caractere"""
+    return "".join(chr(ord(c) ^ chave) for c in texto)
+
+def criptografar(texto, chave_xor):
+    """Criptografa com César (3) + XOR"""
+    cesar = cifra_cesar(texto, 3)
+    return xor(cesar, chave_xor)
+
+def descriptografar(texto, chave_xor):
+    """Descriptografa com XOR + César inverso (3)"""
+    texto_xor = xor(texto, chave_xor)
+    return cifra_cesar(texto_xor, -3)
+
+# ------------------------ PROGRAMA PRINCIPAL ------------------------------
+def main():
+    print("=== Criptografia César (fixo=3) + XOR ===")
+    print("1 - Criptografar")
+    print("2 - Descriptografar")
+    opcao = input("Escolha uma opção")
+
+    mensagem = input("Digite a mensagem (máx 128 caracteres): ")[:128]
+    chave_xor = int(input("Digite a chave XOR (0~255): "))
+
+    if opcao == "1":
+        cript = criptografar(mensagem, chave_xor)
+        print("\nMensagem criptografada:")
+        print(cript.encode("latin1")) #mostra bytes legiveis
+    elif opcao == "2":
+        cript = bytes(mensagem, "latin1").decode("latin1")
+        decript = descriptografar(cript, chave_xor)
+        print("\nMensagem descriptografada:")
+        print(decript)
+    else:
+        print("Opção inválida!")
+
+if __name__ == "__main__":
+    main()
